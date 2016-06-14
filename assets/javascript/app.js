f65a0f6e2db0891d775b14b3f430062e700496e5
Lance Martin <ldmartin7777@gmail.com>
2016-06-13 20:41:14 -0500

    added half of on click for amped up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   $('.a1').on('click',function(){
     var action = 0;
     var drama = 0;
@@ -104,6 +105,47 @@ $(document).ready(function(){
       comedy++;
       q++;
       question2();
+    });
+  });
+
+  $(".a3").on('click',function(){
+    $('#answers, #question, #instructions').empty();
+    $('#question').html('<h1>What is most appealing?</h1>');
+    $('#answers').html('<div class = "col-md-2 col-md-offset-2 a1"><h1>Sports, sports, sports</h1></div><div class = "col-md-2 col-md-offset-1 a2"><h1>Show me some standup</h1></div><div class = "col-md-2 col-md-offset-1 a3"><h1>Boom Boom Bass</h1></div>');
+
+    $('.a1').on('click',function(){
+      var sport = "";
+      $('#answers, #question, #instructions').empty();
+      $('#answers').html('<div class = "col-md-2 col-md-offset-2 a1"></div><div class = "col-md-2 col-md-offset-1 a2"></div><div class = "col-md-2 col-md-offset-1 a3"></div>');
+      $('#question').html('<h1>Which Sport?</h1>');
+      $('.a1').html('<h1>MLB</h1>');
+      $('.a2').html('<h1>NFL</h1>');
+      $('.a3').html('<h1>NBA</h1>');
+      $('.a1').on('click',function(){
+        sport = "mlb";
+        $('#answers, #question, #instructions').empty();
+        $('#instructions').html('<h1>Which Team?</h1>');
+        $('#answers').html('<div class = "col-md-6 col-md-offset-3 a1"><form><div class ="forum-group"><label for ="">Type a team name</label></div><input id = "team" type = "text" style ="color: black; margin-bottom: 10px;"><br><button type="submit" class="btn btn-primary">Submit</button></div></form></div>');
+
+        $('button').on('click',function() {
+          var team = $('#team').val().trim();
+          $('#answers, #question, #instructions').empty();
+          var queryURL = 'https://api.seatgeek.com/2/events?venue.state=TX';
+          $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+            console.log(response);
+          });
+          return false;
+        });
+      });
+    });
+    $('.a2').on('click',function(){
+      console.log('a2 click');
+      $('#answers, #question, #instructions').empty();
+      queryURL =
+      'https://api.seatgeek.com/2/performers?taxonomies.name=comedy';
+      $.ajax({url: queryURL, method: 'GET'}).done(function(response){
+        console.log(response);
+      });
     });
   });
 });
